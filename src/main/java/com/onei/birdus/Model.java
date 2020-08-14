b5b46220d78d17016d4034b7dcf3434bb3cf62b4
Patrick O'Sullivan <patrick.osullivan.ie@gmail.com>
2020-08-14 17:10:41 +0100

    lombokked

--- src/main/java/com/onei/birdus/Model.java
+++ src/main/java/com/onei/birdus/Model.java
@@ -1,9 +1,19 @@
 package com.onei.birdus;
 
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+//A bird sighting
 public class Model {
 
+    //sighting position from the HTML page 1-50
     private String resultNumber;
+    //reference is a unique increasing ID for every sighting e.g IB129006
+    //it is NOT used in equals/compareTo
     private String reference;
     private String date;
     private String commonName;
@@ -11,107 +21,27 @@ public class Model {
     private String count;
     private String location;
     private String county;
-    private String photo;
+    private String photo;   // Boolean string "Yes" or ""
 
-    public Model() {
-    }
+    // A sighting is the the same if the birdName, date and county are the same.
+    // Even
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    public Model(String resultNumber, String reference, String date, String commonName, String scientificName, String count, String location, String county, String photo) {
-        this.resultNumber = resultNumber;
-        this.reference = reference;
-        this.date = date;
-        this.commonName = commonName;
-        this.scientificName = scientificName;
-        this.count = count;
-        this.location = location;
-        this.county = county;
-        this.photo = photo;
-    }
+        Model model = (Model) o;
 
-    public String getResultNumber() {
-        return resultNumber;
-    }
-
-    public void setResultNumber(String resultNumber) {
-        this.resultNumber = resultNumber;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getCommonName() {
-        return commonName;
-    }
-
-    public void setCommonName(String commonName) {
-        this.commonName = commonName;
-    }
-
-    public String getScientificName() {
-        return scientificName;
-    }
-
-    public void setScientificName(String scientificName) {
-        this.scientificName = scientificName;
-    }
-
-    public String getCount() {
-        return count;
-    }
-
-    public void setCount(String count) {
-        this.count = count;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getCounty() {
-        return county;
-    }
-
-    public void setCounty(String county) {
-        this.county = county;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
+        if (date != null ? !date.equals(model.date) : model.date != null) return false;
+        if (commonName != null ? !commonName.equals(model.commonName) : model.commonName != null) return false;
+        return county != null ? county.equals(model.county) : model.county == null;
     }
 
     @Override
-    public String toString() {
-        return "{" +
-                "resultNumber='" + resultNumber + '\'' +
-                ", reference='" + reference + '\'' +
-                ", date='" + date + '\'' +
-                ", commonName='" + commonName + '\'' +
-                ", scientificName='" + scientificName + '\'' +
-                ", count='" + count + '\'' +
-                ", location='" + location + '\'' +
-                ", county='" + county + '\'' +
-                ", photo='" + photo + '\'' +
-                '}';
+    public int hashCode() {
+        int result = date != null ? date.hashCode() : 0;
+        result = 31 * result + (commonName != null ? commonName.hashCode() : 0);
+        result = 31 * result + (county != null ? county.hashCode() : 0);
+        return result;
     }
 }
